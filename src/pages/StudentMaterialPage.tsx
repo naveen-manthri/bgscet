e7149ccb1bc731a53b6d9d8985d2581aefea8dc6
Jithendhar <baldajithendhar@gmail.com>
2026-08-31 01:47:22 +0530

    .

--- src/pages/StudentMaterialPage.tsx
+++ src/pages/StudentMaterialPage.tsx
@@ -1,5 +1,4 @@
 import LetterSections from '../components/letterSections/letterSections';
-import DepartmentSectionHeading from '../components/DepartmentSectionHeading/DepartmentSectionHeading';
 import VisitCampus from '../components/VisitCampus/VisitCampus';
 import { studentMaterialData } from '../data/letters/studentMaterial';
 import './StudentMaterialPage.css';
@@ -7,7 +6,6 @@ import './StudentMaterialPage.css';
 function StudentMaterialPage() {
   return (
     <>
-      <DepartmentSectionHeading title={studentMaterialData.title} className="department-section-heading--default" />
       <LetterSections data={studentMaterialData} />
       <div className="student-material__cta">
         <a className="student-material__button" href="https://vtu.ac.in/en/study-material/">
